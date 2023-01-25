e9e72dbcf32d147fc5e927c513ac321e9b6e24b6
wesley.calle <wesley.calle@idwall.co>
2023-01-25 14:59:41 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/wesleyninaja/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.wesleyninaja.workshopmongo.dto;
+
+import com.wesleyninaja.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    //Vamos criar também um construtor cobrecarregado do userDTO recebendo como argumento um objeto do tipo User para eu ter uma forma automatizada de instanciar um userDTO a partir de User
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
--- src/main/java/com/wesleyninaja/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wesleyninaja/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.wesleyninaja.workshopmongo.resources;
 
 import com.wesleyninaja.workshopmongo.domain.User;
+import com.wesleyninaja.workshopmongo.dto.UserDTO;
 import com.wesleyninaja.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,11 @@ public class UserResource {
     private UserService service;
     @GetMapping
     // ResponseEntity vai encapsular toda uma estruturada necessária pra gente retornar respostas http, com possível cabeçalhos, possíveis erros.
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(service.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+
+        List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
